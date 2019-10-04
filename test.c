8ae68774e54900cf59703789f9df257ccde41490
nrkk <nrkk.1.2.3@gmail.com>
2019-10-04 17:48:33 +0530

    test

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <check.h>
+
+START_TEST (sanity_check)
+{
+    fail_unless(5 == 5, "this should succeed");
+    fail_unless(6 == 5, "this should fail");
+}
+END_TEST
+
+int main(void)
+{
+    Suite *s1 = suite_create("Core");
+    TCase *tc1_1 = tcase_create("Core");
+    SRunner *sr = srunner_create(s1);
+    int nf;
+
+    suite_add_tcase(s1, tc1_1);
+    tcase_add_test(tc1_1, sanity_check);
+
+    srunner_run_all(sr, CK_ENV);
+    nf = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return nf == 0 ? 0 : 1;
+}
